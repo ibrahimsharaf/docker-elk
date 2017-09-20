c5262f15a912f966f6016e853ad8efe13a88325d
Ahmed Hefnawi <ahmed.hefnawi@flyingelephantlab.com>
2017-09-20 17:56:57 +0200

    BELK Stack 6.0.0-beta2

--- application/filebeat/install_filebeat_client.sh
+++ application/filebeat/install_filebeat_client.sh
@@ -3,6 +3,6 @@
 set -e
 
 # https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-installation.html
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.5.1-amd64.deb
-dpkg -i filebeat-5.5.1-amd64.deb
-rm -rf filebeat-5.5.1-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.0.0-beta2-amd64.deb
+dpkg -i filebeat-6.0.0-beta2-amd64.deb
+rm -rf filebeat-6.0.0-beta2-amd64.deb
